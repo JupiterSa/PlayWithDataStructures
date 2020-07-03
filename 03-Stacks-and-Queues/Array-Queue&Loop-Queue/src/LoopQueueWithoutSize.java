777488c9b86a96d7e012ef3cf4dfb22e7cf9424a
MoMo <11@qq>
2020-07-03 17:57:44 +0800

    WithoutSize

--- 03-Stacks-and-Queues/Array-Queue&Loop-Queue/src/LoopQueueWithoutSize.java
+++ 03-Stacks-and-Queues/Array-Queue&Loop-Queue/src/LoopQueueWithoutSize.java
@@ -1,27 +1,56 @@
 public class LoopQueueWithoutSize<E> implements Queue<E> {
 
+    private int front;
+    private int tail;
+
+    private E[] data;
+
+    public LoopQueueWithoutSize (int capacity) {
+        data = (E[])new Object[capacity];
+        front = 0;
+        tail = 0;
+    }
+
+    public LoopQueueWithoutSize () {
+        this(10);
+    }
+
+    @Override
+    public E getFront() {
+        if (isEmpty()) {
+            throw new IllegalArgumentException("The LoopQueue is empty.");
+        }
+        return data[front];
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return front == tail;
+    }
+
+    @Override
+    public int getSize() {
+
+        return 0;
+    }
+
+    public int getCapacity () {
+        return data.length - 1;
+    }
+
     @Override
     public void enQueue(E e) {
 
     }
-
     @Override
     public E deQueue() {
         return null;
     }
 
     @Override
-    public E getFront() {
+    public String toString () {
         return null;
     }
 
-    @Override
-    public boolean isEmpty() {
-        return false;
-    }
-
-    @Override
-    public int getSize() {
-        return 0;
-    }
+    
 }
